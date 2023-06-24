34e9c138a3a6bc3297cce8e1c4f21159c8dc4ead
IDGS-901-19002027 <19002027@alumnos.utleon.edu.mx>
2023-06-24 09:47:38 -0600

    24 de junio del 2023 - GJTM

--- src/app/grupos/iric/iric.component.ts
+++ src/app/grupos/iric/iric.component.ts
@@ -13,6 +13,7 @@ export class IricComponent {
   imageMargin:number=2;
   imageState: boolean = true;
   listFilter:string= '';
+  alumnoTitle!:string;
 
   showImage():void{
     this.imageState =! this.imageState;
@@ -24,22 +25,29 @@ export class IricComponent {
       nombre:'Alex Turner Calvo',
       edad:29,
       correo: 'pedro@gmail.com',
-      foto: "https://img.europapress.es/fotoweb/fotonoticia_20180724103317_1200.jpg"
+      foto: "https://img.europapress.es/fotoweb/fotonoticia_20180724103317_1200.jpg",
+      calif: 6.7
     },
     {
       matricula:1234,
       nombre:'Alex Turner 2013',
       edad:23,
       correo: 'david@gmail.com',
-      foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Alex_Turner_%28musician%29_2011.jpg/220px-Alex_Turner_%28musician%29_2011.jpg"
+      foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Alex_Turner_%28musician%29_2011.jpg/220px-Alex_Turner_%28musician%29_2011.jpg",
+      calif: 7.5
     },
     {
       matricula:1234,
       nombre:'Alex Turner Actual',
       edad:35,
       correo: 'alex@gmail.com',
-      foto: "https://ichef.bbci.co.uk/news/1024/cpsprodpb/83B3/production/_127151733_gettyimages-1421579894.jpg"
+      foto: "https://ichef.bbci.co.uk/news/1024/cpsprodpb/83B3/production/_127151733_gettyimages-1421579894.jpg",
+      calif: 10
     },
-  ]
+  ];
+
+  onCalificaClick(message:string){
+    this.alumnoTitle=`alumnos ${message}`;
+  }
 
 }
